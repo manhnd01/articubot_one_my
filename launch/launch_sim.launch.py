b04c20a07f1a9bb1ef1c4acd816bcfbafde5ad3c
manhprofestion <nguyendomanhtb@gmail.com>
2023-12-09 19:05:21 +0700

    okk ros2 control

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -25,11 +25,12 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
     )
 
-
+    gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
                     
              )
 
