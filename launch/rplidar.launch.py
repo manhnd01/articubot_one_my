0d2ee7c0133efb77390c6ff98f730a7eab426783
manhprofestion <nguyendomanhtb@gmail.com>
2023-12-10 13:04:27 +0700

    okk

--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -20,3 +20,22 @@ def generate_launch_description():
     ])
 
 
+# import os
+# from launch import LaunchDescription
+# from launch_ros.actions import Node
+
+# def generate_launch_description():
+#     return LaunchDescription([
+#         Node(
+#             package='rplidar_ros',
+#             executable='rplidar_composition',
+#             output='screen',
+#             parameters=[
+#                 {'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.3:1.0-port0'},
+#                 {'frame_id': 'laser_frame'},
+#                 {'angle_compensate': True},  # Sửa đổi thành giá trị True, không cần dấu ngoặc kép
+#                 {'scan_mode': 'Standard'},
+#                 {'serial_baudrate': 115200}
+#             ]
+#         )
+#     ])
